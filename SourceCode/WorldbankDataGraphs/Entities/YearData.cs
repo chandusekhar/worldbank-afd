a4792a729c1c2219a87e2496c3aa144e6c2b210d
nqk.mail@gmail.com <nqk.mail@gmail.com@50664097-3ba6-ce17-f079-940d20494fc2>
2010-12-21 07:30:11 +0000

    DataGraph, using dummy data

---
+++ SourceCode/WorldbankDataGraphs/Entities/YearData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace WorldbankDataGraphs.Entities
+{
+    public class YearData
+    {
+        #region getters and setters
+        public int Year { get; set; }
+        public Dictionary<string, string> Attributes
+        {
+            get { return attributes; }
+            set { this.attributes = value; }
+        }
+        #endregion
+
+        #region private vars
+        private Dictionary<string, string> attributes = new Dictionary<string, string>();
+        #endregion
+    }
+}
