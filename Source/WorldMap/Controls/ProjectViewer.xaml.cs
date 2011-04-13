7347fc9c991a8541f95a03cc949387ef5e0805aa
EledraNguyen <EledraNguyen@50664097-3ba6-ce17-f079-940d20494fc2>
2011-04-13 11:59:21 +0000

    Add form for project viewer

---
+++ Source/WorldMap/Controls/ProjectViewer.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace WorldMap
+{
+    public partial class ProjectViewer : UserControl
+    {
+        public ProjectViewer()
+        {
+            InitializeComponent();
+        }
+    }
+}
